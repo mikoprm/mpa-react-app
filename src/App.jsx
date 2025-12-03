d1dd01b5fe6251d9de1e02129f4cd9b391b6d00d
Mikoprn <b4gas24@gmail.com>
2025-12-03 14:06:30 +0700

    change 404

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ const App = () => {
             <Route path="/about" element={<About />} />
             <Route path="/services" element={<Services />} />
             <Route path="/contact" element={<Contact />} />
-            <Route path="/*" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
